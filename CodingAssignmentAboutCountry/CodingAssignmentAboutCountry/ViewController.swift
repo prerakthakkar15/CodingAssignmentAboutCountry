cdc69f63ede954b6541e1592f8e60b41354828cd
Prerak Thakkar <prerak.thakkar@nipponindiaamc.com>
2020-12-21 05:20:17 +0530

    Initial set up - Created Xcode Project

---
+++ CodingAssignmentAboutCountry/CodingAssignmentAboutCountry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingAssignmentAboutCountry
+//
+//  Created by Prerak on 21/12/20.
+//  Copyright © 2020 PrerakHomePC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
